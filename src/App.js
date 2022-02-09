2fff767ff597b7ca7986bb2c215bf53cd95098ab
JA-Lourenco <ja.lourenco@outlook.com>
2022-02-08 21:56:57 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default function App() {
+
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  const [msg, setMsg] = useState('');
+
+  function calcIMC() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if(imc < 16) {
+      setMsg("Classificação: Magreza grau III. Resultado IMC: " + imc.toFixed(2));
+    } else if(imc >= 16 && imc < 16.9) {
+        setMsg("Classificação: Magreza grau II. Resultado IMC: " + imc.toFixed(2));
+    } else if(imc >= 17 && imc < 18.4) {
+        setMsg("Classificação: Magreza grau I. Resultado IMC: " + imc.toFixed(2));
+    } else if(imc >= 18.5 && imc < 24.9) {
+        setMsg("Classificação: Adequado. Resultado IMC: " + imc.toFixed(2));
+    } else if(imc >= 25 && imc < 29.9) {
+        setMsg("Classificação: Pré-obeso. Resultado IMC: " + imc.toFixed(2));
+    } else if(imc >= 30 && imc < 34.9) {
+        setMsg("Classificação: Obesidade grau I. Resultado IMC: " + imc.toFixed(2));
+    } else if(imc >= 35 && imc < 39.9) {
+        setMsg("Classificação: Obesidade grau II. Resultado IMC: " + imc.toFixed(2));
+    } else {
+        setMsg("Classificação: Obesidade grau III. Resultado IMC: " + imc.toFixed(2))
+    }
+  }
+
+  return(
+
+    <div className="app">
+
+      <h1>Calculadora IMC</h1>
+
+      <span>Preencha os campos para calcular o seu IMC</span>
+
+      <div className="area-input">
+
+        <input type="text" 
+        placeholder="Peso em (kg) Ex: 90" 
+        value={peso}
+        onChange={ (e) => setPeso(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        </input>
 
-export default App;
+        <input type="text" 
+        placeholder="Altura em (cm) Ex: 180" 
+        value={altura}
+        onChange={ (e) => setAltura(e.target.value)}
+        >
+        </input>
+
+        <button onClick={calcIMC}>Calcular</button>
+
+      </div>
+
+      <h2>{msg}</h2>
+
+    </div>
+  )
+}
